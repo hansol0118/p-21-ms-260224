a3a5f42a91a972ceb463792dc2db97cbd64f72e8
hansol0118 <wall197944@naver.com>
2026-03-06 18:06:59 +0900

    init

---
+++ jsb2/src/main/java/com/back/jsb2/Jsb2Application.java
@@ -0,0 +1,13 @@
+package com.back.jsb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsb2Application.class, args);
+    }
+
+}
---
+++ jsb2/src/test/java/com/back/jsb2/Jsb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.jsb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
